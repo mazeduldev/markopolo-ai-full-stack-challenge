e48761bd96e5b23d05f73bf43328d65a3a2239b4
Mazedul Islam <mazidmailbox@gmail.com>
2025-10-01 22:13:45 +0600

    be: cleanup unused entity

--- server/src/campaign/campaign.module.ts
+++ server/src/campaign/campaign.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Campaign } from './entities/campaign.entity';
-import { CampaignChannel } from './entities/campaign-channel.entity';
 import { CampaignService } from './campaign.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Campaign, CampaignChannel])],
+  imports: [TypeOrmModule.forFeature([Campaign])],
   providers: [CampaignService],
 })
 export class CampaignModule {}
--- server/src/campaign/campaign.service.ts
+++ server/src/campaign/campaign.service.ts
@@ -2,15 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Campaign } from './entities/campaign.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CampaignChannel } from './entities/campaign-channel.entity';
 
 @Injectable()
 export class CampaignService {
   constructor(
     @InjectRepository(Campaign)
     private readonly campaignRepository: Repository<Campaign>,
-
-    @InjectRepository(CampaignChannel)
-    private readonly campaignChannelRepository: Repository<CampaignChannel>,
   ) {}
 }
